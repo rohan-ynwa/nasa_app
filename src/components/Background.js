9c4b869efcccb4c78fe1d592cda368a817f564b5
rohan <rohanadla8@gmail.com>
2021-02-23 22:12:05 -0500

    added poster for video

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import stars from './starry.mp4';
+import star from './starry.png';;
 
 function Background() {
   return (
     <video id="background-video" loop autoPlay muted playsInline >
-      <source src={stars} type="video/mp4" poster="starry.png"/>
+      <source src={stars} type="video/mp4" poster={star}/>
       Your browser does not support the video tag.
     </video>
   );
