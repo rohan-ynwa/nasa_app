315d75c364966d0229c80c5b27eb586f65dae3a6
rohan <rohanadla8@gmail.com>
2021-02-24 15:57:30 -0500

    added video fallback

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import stars from './starry.mp4';
-import star from './starry.png';;
+import starvid from './stars.webm';
+import starvid2 from './starry.mp4';
+import starimg from './starry.png';
 
 function Background() {
   return (
-    <video id="background-video" loop autoPlay muted playsInline>
-      <source src={stars} type="video/mp4" poster={star}/>
+    <video id="background-video" loop autoPlay muted playsInline poster={starimg}>
+      <source src={starvid} type="video/webm"/>
+      <source src={starvid2} type="video/mp4"/>
       Your browser does not support the video tag.
     </video>
   );
